2b97d943a2e18d060a9ddd289a62d0193dd03387
Ilya Kamens <ilyakamens@gmail.com>
2023-11-17 17:00:08 -0500

    Customize

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div class="hi">Hi.</div>
   </main>
 </template>
+
+<style scoped>
+  .hi {
+    font-size: 5em;
+    text-align: center;
+    margin-top: 40vh;
+  }
+</style>
