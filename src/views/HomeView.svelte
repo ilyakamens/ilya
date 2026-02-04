3c5b8765e6d6c32deb83a85bf8f17abcb044ed3f
Ilya Kamens <ilyakamens@gmail.com>
2026-02-04 14:37:39 +0100

    Add XKCD

--- src/views/HomeView.svelte
+++ src/views/HomeView.svelte
@@ -8,7 +8,12 @@
     <p>
       <a target="_blank" href="https://stackoverflow.com/a/1732454/814589">
         This
-      </a> is my favorite Stack Overflow response of all time.
+      </a>
+      is my favorite Stack Overflow response of all time. And here are
+      <a target="_blank" href="https://xkcd.com/327/">some</a>
+      <a target="_blank" href="https://xkcd.com/378/">good</a>
+      <a target="_blank" href="https://xkcd.com/386/">XKCD</a>
+      <a target="_blank" href="https://xkcd.com/1172/">comics</a>.
     </p>
   </div>
 </main>
